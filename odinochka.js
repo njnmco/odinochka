77d057a654966a69cdbabed040a68c89ec4a372b
Neal Fultz <nfultz@gmail.com>
2019-12-09 19:37:51 +0000

    float fixes, box model fixes

--- odinochka.js
+++ odinochka.js
@@ -4,6 +4,41 @@ render()
 
 function tabclick(event) {
 
+    console.log(event)
+    {
+        var me = event.target;
+        var ts = parseInt(event.target.parentNode.id);
+        var url = event.target.href;
+
+
+        if(!(event.shiftKey || event.ctrlKey)){
+            window.indexedDB.open("odinochka", 5).onsuccess = function(event){
+                var db = event.target.result;
+
+                var tx = db.transaction('tabgroups', 'readwrite');
+                var store = tx.objectStore('tabgroups');
+
+                store.get(ts).onsuccess = function(event) {
+                    var data = event.target.result;
+
+
+
+                    var i = data.urls.indexOf(url);
+
+                    data.tabs.splice(i, 1)
+                    data.urls = data.tabs.map(a => a.url);
+
+                    store.put(data)
+
+
+                    me.parentNode.removeChild(me)
+                    console.log(event)
+                }
+            }
+        }//shift/ctrl if
+
+    }
+
     return event.clientX > event.target.offsetLeft;
 
 }
@@ -19,19 +54,24 @@ function render() {
         var store = tx.objectStore('tabgroups');
         store.openCursor().onsuccess = function(event) {
                 var cursor = event.target.result;
-                if(!cursor) return;
+                var ddiv = document.createElement("div");
+                if(!cursor){
+                    groupdiv.appendChild(ddiv); // forces floats to clear
+                    return;
+                }
 
                 var data = cursor.value;
 
 
-                var ddiv = document.createElement("div");
                 ddiv.id = data.ts;
 
                 for(var tab of data.tabs) {
                     var a = document.createElement("a");
                     a.innerText = tab.title;
                     a.href = tab.url;
-                    a.style = `--bg-favicon: url("${tab.favicon}")`;
+                    if(tab.favicon){
+                        a.style = `--bg-favicon: url("${tab.favicon}")`;
+                    }
                     a.className = "tab";
                     a.onclick = tabclick;
                     a.target = "_blank";
