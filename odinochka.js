60b9f97cd17594d142d459740892633a5e738605
Neal Fultz <nfultz@gmail.com>
2022-09-13 19:31:41 +0000

    Remove youtube popdown

--- odinochka.js
+++ odinochka.js
@@ -315,55 +315,12 @@ function tabclick(event) {
         return false;
     }
 
-    // Youtube popdown
-    if (event.clientX > me.offsetLeft + me.offsetWidth - 10) {
-        makeyt(me);
-        return false;
-    }
-
     if (event.shiftKey || event.ctrlKey || locked || (restore  == "keep")) {
         return true;
     }
 
     chrome.tabs.create({url:me.href, pinned:me.target == "_pinned"}, t => deleteTabFromGroup(ts, i, me));
     return false;
-}
-
-var ytobserver = null;
-function makeyt(me) {
-   var ytouter = document.getElementById("ytouter");
-   if(ytobserver) ytobserver.disconnect();
-
-   ytVidCode = me.href.replace(/^.*v=/, '').replace(/&.*$/, '')
-   ytouter.innerHTML = `<iframe src=https://www.youtube.com/embed/${ytVidCode}
-       frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-       allowfullscreen ></iframe>`
-
-   //preserve aspect ratio on resize
-   ytouter.children[0].onload = function() {
-       var ws = window.getComputedStyle(ytouter);
-       var ar = 1.1 * Number.parseFloat(ws.height) / Number.parseFloat(ws.width);
-       ytobserver = new MutationObserver((ml, obs) => {
-           var nw = Number.parseFloat(ytouter.style.width);
-           var nh = Number.parseFloat(ytouter.style.height);
-           var calc = Math.round(nw * ar);
-           if (nh != calc) {
-               ytouter.style.setProperty("height", `${calc}px`);
-           }
-       });
-
-       // Start observing the target node for configured mutations
-       ytobserver.observe(ytouter, {attributes: true});
-    };
-
-    ytouter.ondblclick = function() {
-       ytouter.innerHTML = '';
-       ytouter.setAttribute('style', ''); //resets the manual sizing
-       if(ytobserver) ytobserver.disconnect();
-       return false;
-   }
-
-
 }
 
 
